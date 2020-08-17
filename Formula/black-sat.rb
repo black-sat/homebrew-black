c7040b4155fddab83efd7a4e60ca0f16d3c1e0b6
Nicola Gigante <nicola.gigante@gmail.com>
2020-08-17 20:22:26 +0200

    Fix

--- Formula/black-sat.rb
+++ Formula/black-sat.rb
@@ -2,7 +2,7 @@ class BlackSat < Formula
   desc "BLACK (Bounded Lᴛʟ sAtisfiability ChecKer)"
   homepage ""
   url "https://github.com/black-sat/black/archive/v0.1.0.tar.gz"
-  sha256 "104bfdaedb86fb5c9da436b0b60826ba93f9939b887b5b9348edf8a32f0d9998"
+  sha256 "ee0b83d12dd04294591c8eb769f93a42093bd94e8439120069060f51fce0de48"
   license "MIT"
 
   depends_on "cmake" => :build
