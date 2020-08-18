05bb630abee8b98a23c8e01c61df03039ca3c674
Nicola Gigante <nicola.gigante@gmail.com>
2020-08-18 20:59:31 +0200

    New hash

--- Formula/black-sat.rb
+++ Formula/black-sat.rb
@@ -2,7 +2,7 @@ class BlackSat < Formula
   desc "BLACK (Bounded Lᴛʟ sAtisfiability ChecKer)"
   homepage ""
   url "https://github.com/black-sat/black/archive/v0.1.0.tar.gz"
-  sha256 "3be0f12fef7076606748f663f80f21efb6fdaf6d8d2ea1efc6fb65693766cf4e"
+  sha256 "d9168dd873af170118235030afd10c026d329c5b30d47bd76e6253832b7ba0ff"
 
   depends_on "cmake" => :build
   depends_on "hopscotch-map" => :build
