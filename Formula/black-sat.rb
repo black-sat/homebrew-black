7e744f08290ecc2448d8c5f65274f6788d49fafc
Nicola Gigante <nicola.gigante@gmail.com>
2025-03-07 15:02:25 +0100

    Fix

--- Formula/black-sat.rb
+++ Formula/black-sat.rb
@@ -2,7 +2,7 @@ class BlackSat < Formula
   desc "BLACK (Bounded Lᴛʟ sAtisfiability ChecKer)"
   homepage "https://www.black-sat.org"
   url "https://github.com/black-sat/black/archive/v25.03.0.tar.gz"
-  sha256 "19b3c63fec515753dab40b6767683e642450224178b40d476d44eb1d0d7413d7"
+  sha256 "49b5f42c19ac835aae6db500f2a9dbe2af332e129eef6a7fa9fa2625954d3504"
 
   version_scheme 1
 
